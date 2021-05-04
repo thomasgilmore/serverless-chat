97228eede916f2b8f88f4f37b3684639cb5dcfb9
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-05-04 19:12:50 -0400

    add imports to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
     import router from "page";
+    import Auth from "./routes/auth.svelte";
     import Index from "./routes/index.svelte";
+    import NotFound from "./routes/notfound.svelte"
+    import { queryString } from "./services/util";
 
     let page;
     let params = {};
+    let qa = {};
 
     router("/", () => page = Index)
+    router("/404/:msg", (ctx: {params: {}, querystring: string}, next) => {
+        params = ctx.params;
+        next();
+    }, () => page = NotFound)
+    router("/auth", (ctx, next) => {
+        qs = queryString(ctx.querystring)
+        next();
+    }, () => page = Auth)
+    router("/login", () => router.redirect("/auth"))
+    router("/register", () => router.redirect("/auth"))
 
+    router("/*", () => page = NotFound)
     router.start();
 </script>
 
-<svelte:component this={page} {params} />
+<svelte:component this={page} {params} {qs} />
